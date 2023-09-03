c3aa9845e4e7af516c1975d9def10740d52845c5
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-09-03 15:24:40 -0500

    added more tables and improved syntax

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,33 @@
 CREATE DATABASE e_commerce_db;
 
 CREATE TABLE
-    categories(
+    Categories (
         id INT NOT NULL AUTO_INCREMENT,
         category_name VARCHAR(255) NOT NULL,
         PRIMARY KEY (id)
     );
 
 CREATE TABLE
-    product(
+    Product (
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         product_name VARCHAR(255) NOT NULL,
         price DECIMAL(10, 2) NOT NULL,
         stock INT NOT NULL DEFAULT 10,
         categories_id INT,
-        FOREIGN KEY (categories_id) REFERENCES categories(id)
+        FOREIGN KEY (categories_id) REFERENCES Categories(id)
     );
 
 CREATE TABLE
-    tag(
+    Tag (
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        tag_name VARCHAR(255) NOT NULL,
+        tag_name VARCHAR(255) NOT NULL
+    );
+
+CREATE TABLE
+    ProductTag (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        product_id INT,
+        tag_id INT,
+        FOREIGN KEY (product_id) REFERENCES Product(id),
+        FOREIGN KEY (tag_id) REFERENCES Tag(id)
     );
