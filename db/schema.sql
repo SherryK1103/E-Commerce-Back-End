8147dee55a8a1cf23d3a6e72202f3ff60d259c7c
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-09-03 15:27:42 -0500

    just remembered the tables have to be created using the models js files oopsies

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,57 @@
 CREATE DATABASE e_commerce_db;
 
-CREATE TABLE
-    Categories (
-        id INT NOT NULL AUTO_INCREMENT,
-        category_name VARCHAR(255) NOT NULL,
-        PRIMARY KEY (id)
-    );
+-- CREATE TABLE
 
-CREATE TABLE
-    Product (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        product_name VARCHAR(255) NOT NULL,
-        price DECIMAL(10, 2) NOT NULL,
-        stock INT NOT NULL DEFAULT 10,
-        categories_id INT,
-        FOREIGN KEY (categories_id) REFERENCES Categories(id)
-    );
+--     Categories (
 
-CREATE TABLE
-    Tag (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        tag_name VARCHAR(255) NOT NULL
-    );
+--         id INT NOT NULL AUTO_INCREMENT,
 
-CREATE TABLE
-    ProductTag (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        product_id INT,
-        tag_id INT,
-        FOREIGN KEY (product_id) REFERENCES Product(id),
-        FOREIGN KEY (tag_id) REFERENCES Tag(id)
-    );
+--         category_name VARCHAR(255) NOT NULL,
+
+--         PRIMARY KEY (id)
+
+--     );
+
+-- CREATE TABLE
+
+--     Product (
+
+--         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+
+--         product_name VARCHAR(255) NOT NULL,
+
+--         price DECIMAL(10, 2) NOT NULL,
+
+--         stock INT NOT NULL DEFAULT 10,
+
+--         categories_id INT,
+
+--         FOREIGN KEY (categories_id) REFERENCES Categories(id)
+
+--     );
+
+-- CREATE TABLE
+
+--     Tag (
+
+--         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+
+--         tag_name VARCHAR(255) NOT NULL
+
+--     );
+
+-- CREATE TABLE
+
+--     ProductTag (
+
+--         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+
+--         product_id INT,
+
+--         tag_id INT,
+
+--         FOREIGN KEY (product_id) REFERENCES Product(id),
+
+--         FOREIGN KEY (tag_id) REFERENCES Tag(id)
+
+--     );
