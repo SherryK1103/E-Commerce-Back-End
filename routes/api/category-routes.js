6e2ffde91d2bfecde8274be9f8b239747060feb7
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-09-04 08:05:09 -0500

    added code to category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,44 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.json(categories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).jason({ message: 'Internal server error' });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+    const category = await Category.frindByPk(categoryId, {
+      include: [{ model: Product }],
+    });
+
+    if (!category) {
+      return res.status(404).json({ message: 'Category not found' });
+    }
+
+    res.json(category);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(201).json(newCategory);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 router.put('/:id', (req, res) => {
